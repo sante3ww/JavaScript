2c43cdf1df25c5ed1b2053d3ba5ba2c6fd9d4564
Dima <kupriyd11@gmail.com>
2025-03-11 23:22:30 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
-function getRandomNumber() {
-    return Math.floor(Math.random() * 100) + 1;
+const user = {
+    name: "Иван",
+    age: 25,
+    city: "Москва"
+};
+
+
+function printUserInfo(user) {
+    console.log(`Имя: ${user.name}, Возраст: ${user.age}, Город: ${user.city}`);
 }
 
-console.log("Случайное число: " + getRandomNumber());
+
+printUserInfo(user);
