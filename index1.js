e78391e75eb39211c7d2eb147dad6cc392116fbd
Dima <-email@.com>
2025-04-15 23:58:27 +0200

    day2

--- index1.js
+++ index1.js
@@ -1,27 +1,16 @@
-// Статус авторизации
-let isLoggedIn = true;
+// Math object & property Random number
+let randomCubeNumber = Math.floor(Math.random() * 9) + 1
+let print = `Ваше число ${randomCubeNumber}`
+// Functions parseInt & parseFloat
+let numberFromString = parseInt(randomCubeNumber)
+console.log(print)
+console.log(`Число взятое с "print": ${numberFromString}`)
+//
+let string = "3"
+let string2 = string + 1.5
+let numberFromString2 = parseFloat(string2)
+console.log(typeof string2, string2)
+console.log(typeof numberFromString2, numberFromString2)
 
-// Роли
-const ROLE_ADMIN = "admin";
-const ROLE_MODERATOR = "moderator";
-const ROLE_USER = "user";
 
-// Устанавливаем роль и статус конкретного пользователя
-const userStatus = isLoggedIn;
-const userRole = ROLE_MODERATOR;
-
-// Проверка доступа
-if (userStatus) {
-    if (userRole === ROLE_ADMIN) {
-        console.log("Добро пожаловать, Админ!");
-    } else if (userRole === ROLE_MODERATOR) {
-        console.log("Добро пожаловать, Модератор!");
-    } else if (userRole === ROLE_USER) {
-        console.log("Добро пожаловать, Пользователь!");
-    } else {
-        console.log("Неизвестная роль.");
-    }
-} else {
-    console.log("Вам нужно авторизироваться!");
-}
 
---
+++ index2.js
@@ -0,0 +1,27 @@
+// Статус авторизации
+let isLoggedIn = true;
+
+// Роли
+const ROLE_ADMIN = "admin";
+const ROLE_MODERATOR = "moderator";
+const ROLE_USER = "user";
+
+// Устанавливаем роль и статус конкретного пользователя
+const userStatus = isLoggedIn;
+const userRole = ROLE_MODERATOR;
+
+// Проверка доступа
+if (userStatus) {
+    if (userRole === ROLE_ADMIN) {
+        console.log("Добро пожаловать, Админ!");
+    } else if (userRole === ROLE_MODERATOR) {
+        console.log("Добро пожаловать, Модератор!");
+    } else if (userRole === ROLE_USER) {
+        console.log("Добро пожаловать, Пользователь!");
+    } else {
+        console.log("Неизвестная роль.");
+    }
+} else {
+    console.log("Вам нужно авторизироваться!");
+}
+
